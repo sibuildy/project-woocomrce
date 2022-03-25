c2ca4a7086a8412e1e5b57dd2d9df1cd12f6123c
ADMIN\Admin <si.buildy@gmail.com>
2022-03-25 16:53:52 +0700

    commit tuong lai

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\OrdersController;
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ReportController;
-
+use App\Http\Controllers\admin\AttributesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,16 +29,19 @@ Route::group(['prefix' => 'admin'], function () {
 
 });
 
+
 Route::prefix('admin')->name('admin.')->group(function (){
 
 
-    Route::prefix('Category')->name('Category')->group(function (){
+    Route::prefix('category')->name('Category')->group(function (){
         Route::get('/', [CategoryController::class, 'index'])->name('select');
         Route::get('delete/{id}', [CategoryController::class, 'delete'])->name('delete');
         Route::get('create', [CategoryController::class, 'create'])->name('create');
         Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('update');
         // Route::get('store', [CategoryController::class, 'store'])->name('store');
         Route::post('store', [CategoryController::class, 'store'])->name('store');
+
+
     });
     Route::prefix('Product')->name('Product')->group(function (){
         Route::get('/', [ProductController::class, 'index'])->name('select');
@@ -49,9 +52,15 @@ Route::prefix('admin')->name('admin.')->group(function (){
         Route::get('/edit', function () {
             return redirect('/admin/Product/');
         });
-
-
     });
+
+    Route::prefix('attributes')->name('attributes')->group(function (){
+        Route::get('/', [AttributesController::class, 'index'])->name('select');
+        Route::get('delete/{id}', [AttributesController::class, 'delete'])->name('delete');
+        Route::get('create', [AttributesController::class, 'create'])->name('create');
+        Route::get('edit/{id}', [AttributesController::class, 'edit'])->name('update');
+    });
+
 
     Route::prefix('orders')->name('orders')->group(function (){
         Route::get('/', [OrdersController::class, 'index'])->name('select');
